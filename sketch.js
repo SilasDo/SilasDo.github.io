921395ae09ecbe963c2e4f796fbc6047b4356a5e
Silas Dohm <silasdo23@gmail.com>
2019-04-09 13:37:27 +0200

    stuff Oo

--- sketch.js
+++ sketch.js
@@ -1,35 +1,36 @@
 let balls = new Array();
 let farben = ['#4717F6', '#A239CA', '#DC7633', '#186A3B', '#0101DF', '#DF013A', '#D7DF01', '#FAFAFA', '#2EFE2E']
+speed = 5;
 function setup() {
-    var cnv = createCanvas(windowWidth,windowHeight);
+    var cnv = createCanvas(windowWidth, windowHeight);
     cnv.style('display', 'block');
 
-    for (let i = 0; i < 10; i++) {
-        balls.push(new mas(i * 50 + 50, 100, 1, random(-1, 1), random(-1, 1)));
+
+    // fckwrt();
+    //balls.push(new mas(0 + width / 25, height / 2 - width / 100, 1, 1, 1))
+    for (let i = 0; i < 14; i++) {
+        balls.push(new mas(i * width/25 + width/25,height/2 -width/100, 1,random(-1,1), random(-1,1)))
     }
-    //balls[0].pos.y=100;
-    //balls.push(new mas(500, 1, 0, 0));
 }
 
 function draw() {
+    //noLoop();
+    background(15);
 
-    background(22);
-    for (let i = 0; i < balls.length; i++) {
-        //speed
-        for (let k = 0; k < 3; k++) {
+    for (let k = 0; k < speed; k++) {
+
+
+        for (let i = 0; i < balls.length; i++) {
+
             balls[i].move();
+
+
             //collisions
             for (let j = i + 1; j < balls.length; j++) {
                 let dis = balls[i].pos.dist(balls[j].pos);
 
 
                 if (dis <= balls[i].d) {
-
-
-                    if (samecolor() && i == 0) {
-                        balls[0].c = Math.round(random(0, farben.length - 1));
-                    }
-
 
                     //elastic collision
                     let m1 = balls[i].m; let m2 = balls[j].m;
@@ -55,17 +56,17 @@ function draw() {
                     xsub.mult(s1);
                     balls[j].v = p5.Vector.sub(v2, xsub);
                     balls[j].c = balls[i].c;
+                    if (samecolor() && i == 0) {
+                        balls[0].c = Math.round(random(0, farben.length - 1));
+                    }
                 }
             }
         }
 
-        balls.forEach(element => {
-            element.draw();
-        });
-
     }
-
-    //noLoop();
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].draw();
+    }
 }
 
 function samecolor() {
@@ -80,7 +81,7 @@ function samecolor() {
 class mas {
     constructor(x, y, m, vx, vy) {
         this.m = m;
-        this.d = 20
+        this.d = width / 50;
         this.pos = createVector(x, y);
         this.v = createVector(vx, vy);
         this.c = Math.round(random(0, farben.length - 1));
@@ -94,6 +95,22 @@ class mas {
     }
     draw() {
         fill(farben[this.c]);
+        //fill('#A239CA');
         ellipse(this.pos.x, this.pos.y, this.d, this.d);
     }
+}
+function fckwrt() {
+    //F
+    balls.push(new mas(50, 300, 1, random(-1, 1), random(-1, 1)));
+    balls.push(new mas(50, 270, 1, random(-1, 1), random(-1, 1)));
+    balls.push(new mas(50, 240, 1, random(-1, 1), random(-1, 1)));
+    balls.push(new mas(50, 210, 1, random(-1, 1), random(-1, 1)));
+    balls.push(new mas(50, 180, 1, random(-1, 1), random(-1, 1)));
+
+    balls.push(new mas(80, 180, 1, random(-1, 1), random(-1, 1)));
+    balls.push(new mas(110, 180, 1, random(-1, 1), random(-1, 1)));
+    balls.push(new mas(140, 180, 1, random(-1, 1), random(-1, 1)));
+    balls.push(new mas(80, 240, 1, random(-1, 1), random(-1, 1)));
+    balls.push(new mas(110, 240, 1, random(-1, 1), random(-1, 1)));
+    balls.push(new mas(140, 240, 1, random(-1, 1), random(-1, 1)));
 }
